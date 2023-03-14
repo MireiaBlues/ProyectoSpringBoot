7b621f8ba6d03fc4a66d1ed11e58319d8f26039e
MireiaBlues <123487100+MireiaBlues@users.noreply.github.com>
2023-03-14 19:33:42 +0100

    Pequeños cambios sobre error

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,19 +2,27 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { SpringApplication.run(DemoApplication.class, args);}
 
-		SpringApplication.run(DemoApplication.class, args);
+	@RequestMapping("/")
+	String hellow() { return "Hello World!";}
+
+	@RequestMapping("/hola")
+	String holam()
+	{
+		return "Hola Mundo!";
 	}
 
-	@RestController
-
-
-
-
+	@RequestMapping("/mimedicacion")
+	String mimedicacion()
+	{
+		return "¡Tómate las pastillas!";
+	}
 }
